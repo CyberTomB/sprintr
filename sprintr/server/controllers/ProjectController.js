0823989ac8ee97e33ee819e88ea0b561fdf8de71
CyberDack <afntwrk.guyrpg@gmail.com>
2021-08-09 15:17:27 -0600

    delete and get requests

--- sprintr/server/controllers/ProjectController.js
+++ sprintr/server/controllers/ProjectController.js
@@ -4,13 +4,33 @@ export class ProjectController extends BaseController {
   constructor() {
     super('project')
     this.router
+      .get('', this.getAll)
       .post('', this.create)
+      .delete('/:id', this.destroy)
+  }
+
+  async getAll(req, res, next) {
+    try {
+      const project = await projectService.getAll(req.query)
+      res.send(project)
+    } catch (error) {
+      next(error)
+    }
   }
 
   async create(req, res, next) {
     try {
       const project = await projectService.create(req.body)
       res.send(project)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async destroy(req, res, next) {
+    try {
+      const project = await projectService.destroy(req.params.id)
+      res.send({ message: `Project ${req.params.id} deleted` })
     } catch (error) {
       next(error)
     }
--- sprintr/server/services/ProjectService.js
+++ sprintr/server/services/ProjectService.js
@@ -1,6 +1,22 @@
 import { dbContext } from '../db/DbContext.js'
-
+import { BadRequest } from '../utils/Errors'
 class ProjectService {
+  async getById(id) {
+    const project = await dbContext.Project.findById(id)
+    if (!project) {
+      throw new BadRequest('invalid id')
+    }
+  }
+
+  async destroy(id) {
+    await this.getById(id)
+    return await dbContext.Project.findByIdAndDelete(id)
+  }
+
+  async getAll(query = {}) {
+    return await dbContext.Project.find(query)
+  }
+
   async create(body) {
     return await dbContext.Project.create(body)
   }
