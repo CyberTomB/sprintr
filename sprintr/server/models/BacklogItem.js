55917e9babba6982eae5a75ef63b1b2abb5b4bf1
SamanthaBullington <Samantha.J.Bullington@gmail.com>
2021-08-10 12:18:53 -0600

    ref backlog

--- sprintr/server/models/BacklogItem.js
+++ sprintr/server/models/BacklogItem.js
@@ -7,8 +7,8 @@ const BacklogItem = new Schema(
     name: { type: String, required: true },
     body: { type: String, required: true },
     isOpen: { type: Boolean, required: true },
-    projectId: { type: String, required: true },
-    creatorId: { type: String, required: true }
+    projectId: { type: String, ref: 'Project', required: true },
+    creatorId: { type: String, ref: 'Account', required: true }
   },
   { timestamps: true, toJSON: { virtuals: true } }
 )
