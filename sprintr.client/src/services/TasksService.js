43b2efc97e837fb6cfc2032d928406e639618800
SamanthaBullington <Samantha.J.Bullington@gmail.com>
2021-08-11 21:29:09 -0600

    added form/creatfn bcklogdetailpage & tasksservice

--- sprintr.client/src/services/TasksService.js
+++ sprintr.client/src/services/TasksService.js
@@ -1,5 +1,6 @@
 import { AppState } from '../AppState'
 import { api } from './AxiosService'
+import Pop from '../utils/Notifier'
 
 class TasksService {
   // REVIEW: How do I make this RESTful?
@@ -7,6 +8,16 @@ class TasksService {
     const res = await api.get('api/backlog/' + backlogId + '/tasks')
     console.log(res.data)
     AppState.tasks = res.data
+  }
+
+  async create(newTask) {
+    try {
+      const res = await api.post('api/backlog', newTask)
+      AppState.tasks.push(res.data)
+      return res.data.id
+    } catch (error) {
+      Pop.toast(error)
+    }
   }
 }
 
