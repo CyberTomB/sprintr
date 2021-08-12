aa9c87c775f2be511c95f6915aa3583913397aaa
SamanthaBullington <Samantha.J.Bullington@gmail.com>
2021-08-12 15:01:46 -0600

    task card update

--- sprintr.client/src/components/TaskCard.vue
+++ sprintr.client/src/components/TaskCard.vue
@@ -16,9 +16,6 @@
       <i class="action mdi mdi-delete text-danger" @click="deleteTask(task.id)"></i>
     </div>
   </div>
-
-  <!-- TODO: This is what a task object will look like. It must accept task objects as the prop from the BacklogItemDetailsPage to render the unique elements from each task. -->
-  <!-- <h3>{{ task.name }}</h3> -->
 </template>
 
 <script>
