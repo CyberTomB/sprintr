8ef254ce8370ec0ef9953b38cee51165ef57ed72
CyberDack <afntwrk.guyrpg@gmail.com>
2021-08-11 20:00:53 -0600

    backlog pulls up tasks

--- sprintr.client/src/pages/BacklogDetailsPage.vue
+++ sprintr.client/src/pages/BacklogDetailsPage.vue
@@ -1,17 +1,19 @@
 <template>
   <h1>TASKS GO HERE</h1>
-  <TaskCard />
+  <!-- <TaskCard /> -->
 </template>
 
 <script>
 import { onMounted } from '@vue/runtime-core'
 import Pop from '../utils/Notifier'
 import { tasksService } from '../services/TasksService'
+import { useRoute } from 'vue-router'
 export default {
   setup() {
+    const route = useRoute()
     onMounted(async() => {
       try {
-        await tasksService.getAllTasks()
+        await tasksService.getAllTasks(route.params.backlog_id)
       } catch (error) {
         Pop.toast(error)
       }
