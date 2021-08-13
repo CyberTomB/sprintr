0bb7c706bc312a5a9ceba86e75478acdf5b9a0c0
CyberDack <afntwrk.guyrpg@gmail.com>
2021-08-13 14:04:47 -0600

    reverted mdi style

--- sprintr.client/src/components/DeleteBtn.vue
+++ sprintr.client/src/components/DeleteBtn.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- NOTE: This button "emits" a "delete" event when clicked. When using this component, make sure to write an "@delete" property in the parent that handles what this event does -->
-  <i class="action mdi mdi-delete text-danger btn btn-outline" @click="$emit('delete')" :title="'Delete ' + itemName"><slot></slot></i>
+  <i class="action mdi mdi-trash-can-outline text-danger btn btn-outline" @click="$emit('delete')" :title="'Delete ' + itemName"><slot></slot></i>
 </template>
 
 <script>
