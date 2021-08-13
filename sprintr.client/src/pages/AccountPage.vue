9d96f9eb32cd9ace601f8878a8a66b2dc6f01dd0
CyberDack <afntwrk.guyrpg@gmail.com>
2021-08-13 10:07:21 -0600

    updated account page

--- sprintr.client/src/pages/AccountPage.vue
+++ sprintr.client/src/pages/AccountPage.vue
@@ -1,6 +1,19 @@
 <template>
-  <div class="about text-center">
-    <h1>Welcome {{ account.name }}</h1>
+  <div class="row justify-content-center">
+    <div class="col-6 my-4" style="width: 18rem;">
+      <img class="card-img-top" :src="account.picture" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">
+          {{ account.name }}
+        </h5>
+        <p class="card-text">
+          Runnr User Since: {{ createdDate }}
+        </p>
+        <router-link :to="{ name: 'ProjectList' }" class="btn btn-primary" title="Projects">
+          My Projects
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,7 +24,11 @@ export default {
   name: 'Account',
   setup() {
     return {
-      account: computed(() => AppState.account)
+      account: computed(() => AppState.account),
+      createdDate: computed(() => {
+        const d = new Date(AppState.account.createdAt)
+        return new Intl.DateTimeFormat('en-US').format(d)
+      })
     }
   }
 }
@@ -19,6 +36,7 @@ export default {
 
 <style scoped>
 img {
-  max-width: 100px;
+  max-height: 30vh;
+  max-width: 30vh;
 }
 </style>
